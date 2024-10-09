5f2e0af96570573bf17ef448c6c8d0cc23038b19
dhemison <dhemis236@gmail.com>
2024-10-09 13:06:45 -0300

    Recapitulando as aulas

--- tuples/Tuplas.py
+++ tuples/Tuplas.py
@@ -72,34 +72,39 @@ Digamos que você está analisando as vendas do Banco de Dados de um e-commerce.
 Em um determinado dia, você extraiu as vendas do Banco de Dados e elas vieram nesse formato:'''
 
 
-# vendas = [
-#     ('20/08/2020', 'iphone x', 'azul', '128gb', 350, 4000),
-#     ('20/08/2020', 'iphone x', 'prata', '128gb', 1500, 4000),
-#     ('20/08/2020', 'ipad', 'prata', '256gb', 127, 6000),
-#     ('20/08/2020', 'ipad', 'prata', '128gb', 981, 5000),
-#     ('21/08/2020', 'iphone x', 'azul', '128gb', 397, 4000),
-#     ('21/08/2020', 'iphone x', 'prata', '128gb', 1017, 4000),
-#     ('21/08/2020', 'ipad', 'prata', '256gb', 50, 6000),
-#     ('21/08/2020', 'ipad', 'prata', '128gb', 4000, 5000),
-# ]
+vendas = [
+    ('20/08/2020', 'iphone x', 'azul', '128gb', 350, 4000),
+    ('20/08/2020', 'iphone x', 'prata', '128gb', 1500, 4000),
+    ('20/08/2020', 'ipad', 'prata', '256gb', 127, 6000),
+    ('20/08/2020', 'ipad', 'prata', '128gb', 981, 5000),
+    ('21/08/2020', 'iphone x', 'azul', '128gb', 397, 4000),
+    ('21/08/2020', 'iphone x', 'prata', '128gb', 1017, 4000),
+    ('21/08/2020', 'ipad', 'prata', '256gb', 50, 6000),
+    ('21/08/2020', 'ipad', 'prata', '128gb', 4000, 5000),
+]
 
 
 '''Qual foi o faturamento de IPhone no dia 20/08/2020?
 Qual foi o produto mais vendido (em unidades) no dia 21/08/2020?'''
-# produ = []
-#
-# faturamento = 0
-# for venda in vendas:
-#     data,produto,cor,capacidade,unidades,preco = venda
-#     if data == '20/08/2020' and produto == 'iphone x':
-#         faturamento += preco*unidades
-#         faturamento_ipad = 'faturamento de {} na data {} foi de {}'.format(produto,data,faturamento )
-#
-#     if data == '21/08/2020':
-#         produ.append(preco)
-#         mais_vendido = 'Produto {} foi o mais vendido, {} Unidades '.format(produto, unidades)
-#
-# print('\n',faturamento_ipad,'\n''\n', mais_vendido)
+
+faturamento = 0
+
+for data, produto, cor, capacidade, unidades, valor in vendas:
+    if "iphone" in produto and data ==  "20/08/2020":
+        faturamento += unidades * valor
+# print("O faturamento de iphone no dia 20/08/2020 foi de",faturamento)
+
+
+quantidade_mais_vendido = 0
+produto_mais_vendido = ''
+for data, produto, cor, capacidade, unidades, valor in vendas:
+    if "21/08/2020" in data:
+        if unidades > quantidade_mais_vendido:
+            produto_mais_vendido = produto
+            quantidade_mais_vendido = unidades
+
+
+# print('O meu produto mais vendido foi {} com {} unidades vendidas'.format(produto_mais_vendido, quantidade_mais_vendido))
 
 
 
@@ -114,20 +119,23 @@ Nesse exercício vamos fazer uma "análise simples" de atingimento de Meta.
 Temos uma lista com os vendedores e os valores de vendas e queremos identificar (printar)
 quais os vendedores que bateram a meta e qual foi o valor que eles venderam.'''
 
-# meta = 10000
-# vendas = [
-#     ('João', 15000),
-#     ('Julia', 27000),
-#     ('Marcus', 9900),
-#     ('Maria', 3750),
-#     ('Ana', 10300),
-#     ('Alon', 7870),
-# ]
-#
-# for vendedor, valor_vendido in vendas:
-#     if valor_vendido >= meta:
-#         print(vendedor,' >> ', f'{valor_vendido:,}')
+meta = 10000
+vendas = [
+    ('João', 15000),
+    ('Julia', 27000),
+    ('Marcus', 9900),
+    ('Maria', 3750),
+    ('Ana', 10300),
+    ('Alon', 7870),
+]
+contador = int()
+bateram_meta = ''
+for vendedor, valor in vendas:
+    if valor >= meta:
+        bateram_meta += "O vendedor {} bateu a meta vendendo {} reais\n".format(vendedor,valor)
+        contador += valor
 
+# print(bateram_meta, contador,"Totais vendidos")
 
 
 
@@ -145,15 +153,18 @@ A lógica da tupla é: (produto, vendas2019, vendas2020)'''
 
 vendas_produtos = [('iphone', 558147, 951642), ('galaxy', 712350, 244295), ('ipad', 573823, 26964), ('tv', 405252, 787604), ('máquina de café', 718654, 867660), ('kindle', 531580, 78830), ('geladeira', 973139, 710331), ('adega', 892292, 646016), ('notebook dell', 422760, 694913), ('notebook hp', 154753, 539704), ('notebook asus', 887061, 324831), ('microsoft surface', 438508, 667179), ('webcam', 237467, 295633), ('caixa de som', 489705, 725316), ('microfone', 328311, 644622), ('câmera canon', 591120, 994303)]
 
-for produto,vendas19,vendas20 in vendas_produtos:
-    crescimento = (vendas20 / vendas19 - 1)
-    if vendas20 > vendas19:
-        print('O produto {} foi maior em 2020 com {} unidades e {} 2019, com o crescimento de {:.1%}.'.format(produto, vendas20,vendas19, crescimento))
+totais_19_20 = 0
+quantidade = 0
+texto = ''
+for produto, vendas_19, vendas_20 in vendas_produtos:
+    if vendas_20 > vendas_19:
+        crescimento = (vendas_20 / vendas_19 - 1)
+        totais_19_20 += valor
+        quantidade += 1
+        texto += "O produto {} foi o mais vendido em 2020. Tendo {:.1%} de crescimento\n".format(produto, crescimento)
 
 
-
-
-
+print(texto)
 
 
 
